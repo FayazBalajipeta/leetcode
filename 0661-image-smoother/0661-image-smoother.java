436496de32f46588b6dd27bbc63f17e10c6a6ecb
FayazBalajipeta <fayazbalajipeta786@gmail.com>
2025-05-19 14:00:22 +0530

    Time: 203 ms (6.59%), Space: 45.4 MB (96.14%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+        int[][] result = new int[m][n];
+
+        // Directions for the 3x3 neighborhood (including the center)
+        int[] dx = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                int count = 0;
+
+                for (int d = 0; d < 9; d++) {
+                    int ni = i + dx[d];
+                    int nj = j + dy[d];
+
+                    if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                        sum += img[ni][nj];
+                        count++;
+                    }
+                }
+
+                result[i][j] = sum / count;  
+            }
+        }
+
+        return result;
+    }
+}
