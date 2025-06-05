589d03c4bd2c3779c7c55a4120b92c7bd84bc442
FayazBalajipeta <fayazbalajipeta786@gmail.com>
2025-06-05 13:38:36 +0530

    Time: 4 ms (12.33%), Space: 41.6 MB (91.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,29 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        // Create hashmap to store the pairs...
-        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
-        Hmap.put(')','(');
-        Hmap.put('}','{');
-        Hmap.put(']','[');
-        // Create stack data structure...
-        Stack<Character> stack = new Stack<Character>();
-        // Traverse each charater in input string...
-        for (int idx = 0; idx < s.length(); idx++){
-            // If open parentheses are present, push it to stack...
-            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
-                stack.push(s.charAt(idx));
-                continue;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+
+            if (!stack.isEmpty()) {
+                char last = stack.peek();
+                if (isPair(last, current)) {
+                    stack.pop(); // matched pair, so pop
+                    continue;
+                }
             }
-            // If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
-            // If not, we need to return false...
-            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
-                return false;
-            }
+
+            stack.push(current);
         }
-        // If the stack is empty, return true...
-        if (stack.size() == 0) {
-            return true;
-        }
-        return false;
+
+        return stack.isEmpty(); 
+    }
+
+    private boolean isPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
     }
 }
